30479e0e7278434a1ad300ae98126ea76e82f21c
Aidan B <aidanrichbennett+github@gmail.com>
2019-11-01 22:46:57 -0400

    first commit, almost complete CFA lunch menu

---
+++ find.py
@@ -0,0 +1,48 @@
+import itertools
+
+target = float(input("Meal price: $"))
+maxitems = int(input("Max items: "))
+tolerance = int(input("Tolerance: ¢"))
+
+for targetoffset in range(tolerance + 1):
+    print("Target: $" + str(target))
+    pricestext = open("prices.txt").read()
+    priceslist = pricestext.split("\n")
+    pricesarray = []
+    for item in priceslist:
+        if("#" not in item.split(": ")[0]):
+            pricesarray += [item.split(": ")]
+
+    pricesarray.pop(-1)
+
+    print("Prices imported")
+
+    exactmeals = []
+    for j in range(maxitems):
+        i = j + 1
+        print("\nGenerating meals with " + str(i) + " items...")
+        meals = list(itertools.combinations(pricesarray, i))
+        for meal in meals:
+            price = 0.0
+            mealincludes = []
+            valid = True
+            for item in meal:
+                if(">" in item[1]):
+                    includes = item[1].split(">")[1].split(" ")[0]
+                    if(includes in mealincludes):
+                        valid = False
+                    else:
+                        mealincludes += [includes]
+                if("<" in item[1]):
+                    if(item[1].split("<")[1].split(" ")[0] not in mealincludes):
+                        valid = False
+                price += float(item[0])
+            if(price == target and valid):
+                exactmeals += [meal]
+                print("\nMeal found:")
+                for item in meal:
+                    print(" -- $" + item[0] + ": " + item[1])
+
+    target -= 0.01
+
+#    exactmeals += list(itertools.combinations(pricesarray, i))
