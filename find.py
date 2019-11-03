def68ad5faae2fa3a0c7885544581daaa8dd942a
Aidan B <aidanrichbennett+github@gmail.com>
2019-11-02 23:43:24 -0400

    Commented the script, did a little bit of bug fixing

--- find.py
+++ find.py
@@ -1,10 +1,12 @@
 import itertools
 
+# Get variables to work with
 target = float(input('Meal price: $'))
 maxitems = int(input('Max items: '))
 tolerance = int(input('Tolerance: $0.0'))
 filename = input('Prices file: ./')
 
+# Read the prices from a text file
 pricestext = open(filename).read()
 priceslist = pricestext.split('\n')
 pricesarray = []
@@ -14,28 +16,56 @@ for item in priceslist:
 
 print('Prices imported')
 
+# Array of all exact meals
+exactmeals = []
+
+# Run the program once for every available price, based on the tolerance
 for targetoffset in range(tolerance + 1):
+    # Print current target price
     print('\n\n    Target: $' + str(target) + '\n_____________________________')
-    exactmeals = []
+    # Find all meals with up to the given number of items
     for j in range(maxitems):
+        # Start counting at 1
         i = j + 1
         print('\nGenerating meals with ' + str(i) + ' items...')
+        # Generate a list of all possible combinations with the menu
         meals = list(itertools.combinations(pricesarray, i))
+
+        # Go through every meal
         for meal in meals:
+            # Start with a price pf 0
             price = 0.0
+            # Start with no base includes (one allowed in a category, allows condiments)
             mealincludes = []
+            # Valid unless proven otherwise
             valid = True
+
+            # For every item in the meal:
             for item in meal:
-                if('>' in item[1]):
-                    includes = item[1].split('>')[1].split(' ')[0]
-                    if(includes in mealincludes):
-                        valid = False
-                    else:
-                        mealincludes += [includes]
+                # If it doesn't depend on an include:
+                if('<' not in item[1]):
+                    # If it's an include:
+                    if('>' in item[1]):
+                        # Find the include
+                        includes = item[1].split('>')[1].split(' ')[0]
+                        # If there's already one there, meal is invalid
+                        if(includes in mealincludes):
+                            valid = False
+                        else:
+                            # Add the include to the includes
+                            mealincludes += [includes]
+                    # Add the price to the total
+                    price += float(item[0])
+            for item in meal:
+                # If it depends on an include:
                 if('<' in item[1]):
+                    # If the include isn't in the meal, it's invalid
                     if(item[1].split('<')[1].split(' ')[0] not in mealincludes):
                         valid = False
-                price += float(item[0])
+                    # Add the price to the total
+                    price += float(item[0])
+
+            # If the meal matches the target and it's valid, print it
             if(price == target and valid):
                 exactmeals += [meal]
                 print('\n$' + str(target) + ' meal found:')
@@ -43,5 +73,3 @@ for targetoffset in range(tolerance + 1):
                     print(' -- $' + item[0] + ': ' + item[1])
 
     target -= 0.01
-
-#    exactmeals += list(itertools.combinations(pricesarray, i))
