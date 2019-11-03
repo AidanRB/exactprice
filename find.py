b39c9a0b2ac27e6df9b7ef6f0005badce4efff27
Aidan B <aidanrichbennett+github@gmail.com>
2019-11-02 22:59:07 -0400

    Changed "s to 's, added ignorance of blank lines in price files

--- find.py
+++ find.py
@@ -1,47 +1,46 @@
 import itertools
 
-target = float(input("Meal price: $"))
-maxitems = int(input("Max items: "))
-tolerance = int(input("Tolerance: $0.0"))
-filename = input("Prices file: ./")
+target = float(input('Meal price: $'))
+maxitems = int(input('Max items: '))
+tolerance = int(input('Tolerance: $0.0'))
+filename = input('Prices file: ./')
 
 pricestext = open(filename).read()
-priceslist = pricestext.split("\n")
+priceslist = pricestext.split('\n')
 pricesarray = []
 for item in priceslist:
-    if("#" not in item.split(": ")[0]):
-        pricesarray += [item.split(": ")]
-pricesarray.pop(-1)
+    if('#' not in item.split(': ')[0] and item != ''):
+        pricesarray += [item.split(': ')]
 
-print("Prices imported")
+print('Prices imported')
 
 for targetoffset in range(tolerance + 1):
-    print("\n\nTarget: $" + str(target) + "__________________")
+    print('\n\nTarget: $' + str(target) + '__________________')
     exactmeals = []
     for j in range(maxitems):
         i = j + 1
-        print("\nGenerating meals with " + str(i) + " items...")
+        print('\nGenerating meals with ' + str(i) + ' items...')
         meals = list(itertools.combinations(pricesarray, i))
         for meal in meals:
             price = 0.0
             mealincludes = []
             valid = True
             for item in meal:
-                if(">" in item[1]):
-                    includes = item[1].split(">")[1].split(" ")[0]
+                if('>' in item[1]):
+                    includes = item[1].split('>')[1].split(' ')[0]
                     if(includes in mealincludes):
                         valid = False
                     else:
                         mealincludes += [includes]
-                if("<" in item[1]):
-                    if(item[1].split("<")[1].split(" ")[0] not in mealincludes):
+                if('<' in item[1]):
+                    if(item[1].split('<')[1].split(' ')[0] not in mealincludes):
                         valid = False
                 price += float(item[0])
             if(price == target and valid):
                 exactmeals += [meal]
-                print("\n$" + str(target) + " meal found:")
+                print('\n$' + str(target) + ' meal found:')
                 for item in meal:
-                    print(" -- $" + item[0] + ": " + item[1])
+                    print(' -- $' + item[0] + ': ' + item[1])
 
     target -= 0.01
 
